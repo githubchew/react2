78df79c76d127262bc7f179006922a271367d454
lmde-git-hoychew <iammusiccchu@gmail.com>
2024-06-29 15:29:08 +0800

    new

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client' 
+import './index.css'
+const firstbook = {
+    author:"jordan moore",
+    title:'interesting facts tbook title',
+    img: './images/book-1.jpg',
+}
+const secondbook = {
+    author:"name 2",
+    title:' facts tbook title',
+    img: './images/book-1.jpg',
+}
 
-// function Greeting(){
-//     return <h2>My first holy moly it works  Cnoway omponenet</h2>
-// }
 
-function Greeting2(){
-    return React.createElement('h2',{}, 'heelo world from Gretting2 function')
+const BookList = () =>{
+    return <section className='booklist'>
+        <Book author={firstbook.author} title={firstbook.title} img={firstbook.img} />
+
+        <Book author={secondbook.author} title={secondbook.title} img={secondbook.img} />
+        <Book />
+        <Book />
+ 
+    </section> 
+}
+
+const Book = (props)=>{
+    console.log(props)
+    return <article className='book'>
+        <img src={props.img} alt='{props.title}' />
+        <h2>{props.title}</h2>
+        <h4>{props.author}</h4>
+    </article>
+}
+
+const Image = ()=> <img src='./images/book-1.jpg' alt='img' />
+const Title = ()=> <h2>Snadwich :klkkl A Novel</h2>
+const Author = ()=> {
+    return <h4 style={{color:'#617d98', fontSize: '0.75rem'}}>Autor:   jk rowling</h4>
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting2 />)
-
-
-
+root.render(<BookList />)
